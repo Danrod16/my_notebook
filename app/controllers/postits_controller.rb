33a3e6e62d57b60745a3f50a56242ad696946424
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-01-18 20:40:26 +0100

    Feat: created the postits for the app missing CRUD

---
+++ app/controllers/postits_controller.rb
@@ -0,0 +1,36 @@
+class PostitsController < ApplicationController
+  def new
+    @postit = Postit.new
+  end
+
+  def create
+    @postit = Postit.new(postit_params)
+    if @postit.save
+      redirect_to postits_path
+      flash[:alert] = "Postit created Modafucka!"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def index
+    @postit = Postit.new
+    @postits = Postit.all
+  end
+
+  private
+
+  def postit_params
+    params.require(:postit).permit(:description)
+  end
+end
